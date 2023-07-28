4b716485594073ffb281fbcc1cb668e1e204841a
Orel Tsioni <oreltsioni@gmail.com>
2023-07-28 11:48:23 +0300

    Changed PCA cards pwm order to align to wiring and reduced recoil time to 40ms

--- main/main.ino
+++ main/main.ino
@@ -9,45 +9,36 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 #define FIRST_NOTE_PITCH 21
 #define OCTAVES 7
 
-const int32_t pca_address[OCTAVES] = {0x45, 0x44, 0x46, 0x41, 0x42, 0x43, 0x40};
+const int32_t pca_address[OCTAVES] = {0x45, 0x44, 0x46, 0x41, 0x42, 0x43, 0x40}; // order of PCA boards across piano
+const int32_t index_mat[12] = {0, 7, 1, 8, 2, 3, 9, 4, 10, 5, 11, 6}; // order of notes across pwm signals of each PCA card
 const int midiChannel = 1;
 const int ledPin = 13;
 
 Adafruit_PWMServoDriver pwm_arr[OCTAVES];
 
-void from_pitch_to_index(uint8_t pitch, int* pca, int* index) {
+void from_pitch_to_index(uint8_t pitch, int* pca, int* index) 
+{
 
   int normalized_pitch =  pitch - FIRST_NOTE_PITCH;
   
-  if ( ( normalized_pitch) && (normalized_pitch  <= 14) ) {
-    // first octave 
-    *pca = 0;
-    *index = normalized_pitch;
+  if ( normalized_pitch >=3 && normalized_pitch <= 86 ) { 
+    // full octaves
+    *pca = (normalized_pitch - 3) / 12;
+    *index = index_mat[(normalized_pitch - 3) % 12];
   } 
   else if (normalized_pitch == 87) 
   {
-      // last note
+    // last note
     *pca = 6;
     *index = 12;
   }
   else
   {
-    *pca = (normalized_pitch - 3) / 12;
-    *index = (normalized_pitch - 3) % 12;
+    //placeholder incase we get wrong values
+    *pca = 0;
+    *index = 0;
   }
-}
 
-
-void press_note(int note, int octave, int force, int note_time_in_ms, Adafruit_PWMServoDriver* pwm_arr) {
-
-  pwm_arr[1].setPWM(note, 0, SOLENOID_ON);
-  delay(10);
-  pwm_arr[1].setPWM(note, 0, force);
-  
-  delay(note_time_in_ms);
-
-  pwm_arr[1].setPWM(note, 0, 0);
-  
 }
 
 void setup() {
@@ -90,7 +81,6 @@ void handleNoteOn(byte channel, byte pitch, byte velocity) {
   if (channel == midiChannel) {
 
     pwm_arr[octave].setPWM(index, 0, SOLENOID_ON);
-   
     // // Turn on the LED
     digitalWrite(ledPin, HIGH);
     // Serial.print("Note On: Pitch = ");
@@ -112,7 +102,7 @@ void handleNoteOff(byte channel, byte pitch, byte velocity) {
   if (channel == midiChannel) {
 
     pwm_arr[octave].setPWM(index, 0, 0);
-
+    delay(40);
     // Turn off the LED
     digitalWrite(ledPin, LOW);
     // Print a message to Serial Monitor
